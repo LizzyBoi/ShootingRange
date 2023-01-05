7e936e4214db4773319550ac427ffdb5a0c59686
AWenneb <asger@wenneberg.net>
2023-01-05 12:43:39 +0100

    change pwm on slider change with bug

--- public/script.js
+++ public/script.js
@@ -1,3 +1,23 @@
+let ws;
+(async function() {
+	ws = await connectToServer();
+
+	ws.onmessage = (wsMessage) => {
+		console.log('recieved message');
+	};
+
+	async function connectToServer() {
+		const ws = new WebSocket('ws://localhost:7071');
+		return new Promise((resolve, reject) => {
+			const timer = setInterval(() => {
+				if(ws.readyState === 1) {
+					clearInterval(timer);
+					resolve(ws);
+				}
+			}, 10);
+		});
+	}
+})();
 
 window.onload = function(){
     const el = document.getElementById("speedInput");
@@ -13,8 +33,14 @@ window.onload = function(){
 
 
 function updateSlider(){
-    document.getElementById('speedSlider').value = document.getElementById('speedInput').value
-    document.getElementById('currentSpeed').innerHTML = document.getElementById('speedInput').value
+    document.getElementById('speedSlider').value = document.getElementById('speedInput').value;
+    document.getElementById('currentSpeed').innerHTML = document.getElementById('speedInput').value;
 }
 
-    
+function onSliderInput() {
+	document.getElementById('currentSpeed').innerText = document.getElementById('speedSlider').value;
+
+	const value = parseInt(document.getElementById('currentSpeed').innerText);
+	const message = {command: "set", value: value};
+	ws.send(JSON.stringify(message));
+}

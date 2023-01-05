6dd027338871bc659378be1abdcf482894e8359b
LizzyBoi <gustavhectorbentzen@gmail.com>
2023-01-05 14:58:06 +0100

    Other input methods than slider sends signal to websocket

--- public/script.js
+++ public/script.js
@@ -36,15 +36,24 @@ function updateSpeed(){
         document.getElementById('speedSlider').value = document.getElementById('speedInput').value;
         document.getElementById('currentSpeed').innerHTML = document.getElementById('speedInput').value;
         document.getElementById('speedInput').value = "";  
+
+        const value = parseInt(document.getElementById('currentSpeed').innerText);
+	    const message = {command: "set", value: value};
+	    ws.send(JSON.stringify(message));
     } else {
         document.getElementById('speedInput').value = "";   
     }
+
 }
 
 function incrementSlider(x){
     var scalar = 2
     document.getElementById('speedSlider').value = document.getElementById('currentSpeed').innerHTML;
     document.getElementById('currentSpeed').innerHTML = parseInt(document.getElementById('currentSpeed').innerHTML) + scalar * x;
+    
+    const value = parseInt(document.getElementById('currentSpeed').innerText);
+	const message = {command: "set", value: value};
+	ws.send(JSON.stringify(message));
 }
 
 function onSliderInput() {

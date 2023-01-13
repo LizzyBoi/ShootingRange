9ff8284f14231acce261ff80de96e0b9415a116b
AWenneb <asger@wenneberg.net>
2023-01-13 11:46:37 +0100

    unfucked motor controller

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,9 @@
 const express = require('express');
 const { Server } = require('ws');
-const Gpio = require('onoff').Gpio;
+const { MotorController } = require('./motorController.js');
 
 const hostname = "127.0.0.1";
 const port = 3000;
-
 
 const ws_server = new Server({ port: 7071 });
 let frontend_ws;
@@ -22,6 +21,10 @@ ws_server.on('connection', (ws) => {
 			set_duty_cycle(message.value);
 		} else if (message.command === "goto") {
 			go_to_position(message.value);
+		} else if (message.command === "stop") {
+			motor_stop();
+		} else if (message.command === "start") {
+			motor_start();	
 		}
 	});
 
@@ -30,128 +33,31 @@ ws_server.on('connection', (ws) => {
 
 
 var app = express()
-	.use(express.static('public'))
+	.use(express.static('../public'))
 	.get('/', (req, res) => {res.send('Hello World')})
 	.listen(port, () => {console.log(`Listening on port ${port}`)});
 
-var pwm_output = [];
-for (const gpio of [6,13,19,26,12,16]) {
-	pwm_output.push(new gpio(gpio, 'out'));	
-}
-var curr_duty_cycle = 32;
+const pwm_pin_numbers = [6,13,19,26,12,16];
+const rotation_pin_number = 20;
+const direction_pin_number = 21;
+const enable_pin_number = 7;
+const emergency_brake_pin_number = 5; 
+var motorController = new MotorController(pwm_pin_numbers, rotation_pin_number, direction_pin_number, enable_pin_number, emergency_brake_pin_number);
 
-var rotation_index = 0;
-
-const enable_signal = new Gpio(7, 'out');
-var enabled = 0;
-
-var  rotation_signal = new Gpio(20, 'in', 'both');
-var direction_signal = new Gpio(21, 'in');
-rotation_signal.watch(rotation_signal_trigger);
 
 function send_to_frontend(object) {
 	frontend_ws.send(JSON.stringify(object));
 }
 
-function brake_motor() {
-	enabled = 0;
-	enable_signal.writeSync(0);	
-	set_duty_cycle(0);
+function change_duty_cycle(value) {
+	motorController.change_duty_cycle(value);	
 }
 
-function enable_motor() {
-	enabled = 1;
-	enable_signal.writeSync(1);
-	set_duty_cycle(32);
+function set_duty_cycle(value) {
+	motorController.set_duty_cycle(value);
 }
 
-function change_duty_cycle(change_by) {
-	curr_duty_cycle = curr_duty_cycle + change_by;
-	if (curr_duty_cycle < 1) {
-		curr_duty_cycle = 1;
-	} else if (curr_duty_cycle > 63) {
-		curr_duty_cycle = 63;
-	}
-
-	pwmDutyCycle(curr_duty_cycle);
+function go_to_position(value) {
+	motorController.go_to_position(value);
 }
 
-function set_duty_cycle(new_duty_cycle) {
-	curr_duty_cycle = new_duty_cycle;
-	pwmDutyCycle(curr_duty_cycle);
-}
-
-function pwmDutyCycle(duty_cycle) {
-	for (let i = 0; i < pwm_output.length; i++) {
-		const mask = 1 << i;
-
-		if (duty_cycle & mask) {
-			pwm_output[i].writeSync(1);
-		} else {
-			pwm_output[i].writeSync(0);
-		}
-	}
-}
-
-function rotation_signal_trigger(err, value) {
-	const direction = direction_signal.readSync() 
-
-	if(direction === 1) {
-		rotation_index++;
-		console.log(rotation_index);
-	} else if (direction === 0) {
-		rotation_index--;
-		console.log(rotation_index);
-	}
-}
-
-//ramp up
-//Idea: take error, and current speed, do some magic with that to either increase or decrease motor speed.
-var interval;
-var running = 0;
-
-function go_to_position(end_position) {
-	console.log(`${Date.now()}\t${curr_duty_cycle}`);
-	if (running) {
-		clearInterval(interval);
-	} else {
-		running = 1;
-	}
-
-	interval = setInterval(set_pwm_controller, 250, end_position);
-}
-
-var reached_position = 0;
-function set_pwm_controller (end_position){
-	//TODO
-	//acutally set enable signal low for breaking
-	const pulse_per_rotation = 2;
-	const rotation_per_cm = 7.5;
-	const curr_position = rotation_index / pulse_per_rotation / rotation_per_cm;
-	const deceleration_distance = 15;
-	if (reached_position) leeway = 0.3;
-	else leeway = 0.2;
-
-	if(end_position + leeway > curr_position && end_position - leeway < curr_position) {
-		console.log("reached approx position");
-		reached_position = 1;
-		set_duty_cycle(32);
-	}
-	
-	var pwm_modifier = (end_position - curr_position) / deceleration_distance;
-	if (pwm_modifier < -1) pwm_modifier = -1;
-	else if (pwm_modifier > 1) pwm_modifier = 1;
-	var pwm_to_send = 32 + Math.ceil(25 * pwm_modifier);
-	if (pwm_to_send > 32 && pwm_to_send < 35) pwm_to_send = 35;
-	if (pwm_to_send > 29 && pwm_to_send < 32) pwm_to_send = 27;
-
-	var pwm_diff = pwm_to_send - curr_duty_cycle;
-	if(pwm_diff > 1) pwm_diff = 1;
-	else if(pwm_diff < -1) pwm_diff = -1;
-	
-	change_duty_cycle(pwm_diff);
-
-	console.log(`${Date.now()}\t${curr_duty_cycle}`);
-}
-
-set_duty_cycle(32);
--- backend/motorController.js
+++ backend/motorController.js
@@ -13,15 +13,19 @@ const MotorController {
 		//set to watch on both edges
 		this.emergency_pin = new Gpio(emergency_pin_number, 'in');;
 
-		this.#rotation_index = 0;
+		this.#curr_rotation_index = 0;
 		this.#curr_duty_cycle = 32;
 
-		const pulse_per_rotation = 4;
-		const rotation_per_cm = 7.5;
-		const curr_position = rotation_index / pulse_per_rotation / rotation_per_cm;
-		const deceleration_distance = 5;
-		if (reached_position) leeway = 0.3;
-		else leeway = 0.2;
+		this.#pulse_per_rotation = 2;
+		this.#rotation_per_cm = 7.5;
+		this.#pulse_per_cm = this.#pulse_per_rotation * this.#rotation_per_cm;
+		this.#deceleration_rotation_distance = 5 * this.#pulse_per_cm;
+		this.#moving_leeway = 0.2 * this.#pulse_per_cm;
+		this.#still_leeway = 0.3 * this.#pulse_per_cm;
+
+		this.#end_rotation_index = 0 * this.#pulse_per_cm;
+		this.#interval = null;
+		this.#reached_position = 0;
 
 
 		this.set_duty_cycle(32);
@@ -31,6 +35,17 @@ const MotorController {
 
 	function enable() {}
 
+	function start() {
+		this.#end_rotation_index = this.#curr_rotation_index;
+		this.#interval = setInterval(this.#control_pwm, 10);
+	}
+
+	function stop() {
+		if(this.#interval) {
+			clearInterval(this.#interval);
+		}
+		this.set_duty_cycle(32);
+	}
 	
 	function change_duty_cycle(change_by) {
 		this.#curr_duty_cycle = #curr_duty_cycle + change_by;
@@ -40,37 +55,31 @@ const MotorController {
 			this.#curr_duty_cycle = 63;
 		}
 
-		this.#pwmDutyCycle(this.#curr_duty_cycle);
+		this.#write_pwm_duty_cycle(this.#curr_duty_cycle);
 	}
 
 	function set_duty_cycle(new_duty_cycle) {
 		this.#curr_duty_cycle = new_duty_cycle;
-		this.#pwmDutyCycle(this.#curr_duty_cycle);
+		this.#write_pwm_duty_cycle(this.#curr_duty_cycle);
 	}
 
 	function go_to_position(end_position) {
-		if(interval) {
-			clearInterval(interval);
-		}
-
-		while(!cancel_move) {
-			interval = setInterval(#set_pwm_controller, 10, end_position);
-		}
+		this.#end_rotation_index = end_position * this.#pulse_per_cm; 
 	}
 
 	function #rotation_trigger(){
 		const direction = this.direction_pin.readSync() 
 
 		if(direction === 1) {
-			this.#rotation_index++;
-			console.log(this.#rotation_index);
+			this.#curr_rotation_index++;
+			console.log(this.#curr_rotation_index);
 		} else if (direction === 0) {
-			this.#rotation_index--;
-			console.log(rotation_index);
+			this.#curr_rotation_index--;
+			console.log(this.#curr_rotation_index);
 		}
 	}
 
-	function #pwmDutyCycle(duty_cycle) {
+	function #write_pwm_duty_cycle(duty_cycle) {
 		for (let i = 0; i < this.pwm_pins.length; i++) {
 			const mask = 1 << i;
 
@@ -82,32 +91,30 @@ const MotorController {
 		}
 	}
 
-	function #set_pwm_controller(end_position) {
+	function #control_pwm() {
 		//TODO
 		//acutally set enable signal low for breaking
-		const pulse_per_rotation = 4;
-		const rotation_per_cm = 7.5;
-		const curr_position = rotation_index / pulse_per_rotation / rotation_per_cm;
-		const deceleration_distance = 5;
-		if (reached_position) leeway = 0.3;
-		else leeway = 0.2;
+		
+		var leeway;
+		if (reached_position) leeway = this.#still_leeway;
+		else leeway = this.#moving_leeway;
 
-		if(end_position + leeway > curr_position && end_position - leeway < curr_position) {
+		if(this.#end_rotation_index + leeway > this.#curr_rotation_index && this.#end_rotation_index - leeway < this.#curr_rotation_index) {
 			console.log("reached approx position");
-			reached_position = 1;
-			set_duty_cycle(32);
+			this.set_duty_cycle(32);
 		}
 
-		var pwm_modifier = (end_position - curr_position) / deceleration_distance;
+		var pwm_modifier = (this.#end_rotation_index - this.#curr_rotation_index) / this.#deceleration_rotation_distance);
 		if (pwm_modifier < -1) pwm_modifier = -1;
 		else if (pwm_modifier > 1) pwm_modifier = 1;
+
 		var pwm_to_send = 32 + Math.ceil(25 * pwm_modifier);
 		if (pwm_to_send > 32 && pwm_to_send < 35) pwm_to_send = 35;
 		if (pwm_to_send > 29 && pwm_to_send < 32) pwm_to_send = 29;
 
-		var pwm_diff = pwm_to_send - curr_duty_cycle;
-		if(pwm_diff > 5) pwm_diff = 5;
-		else if(pwm_diff < -5) pwm_diff = -5;
+		var pwm_diff = pwm_to_send - this.#curr_duty_cycle;
+		if(pwm_diff > 2) pwm_diff = 2;
+		else if(pwm_diff < -2) pwm_diff = -2;
 
 		change_duty_cycle(pwm_diff);
 

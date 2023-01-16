768df2f2bcd5fecc9e7782cd984c43f2a538145a
LizzyBoi <gustavhectorbentzen@gmail.com>
2023-01-16 15:09:08 +0100

    it fucked up tho

--- public/script.js
+++ public/script.js
@@ -123,10 +123,12 @@ function sendCommandToSocket(command) {
 }
 
 function PID_onchange(){
-	document.getElementById(PID_p).value = p_value;
-	document.getElementById(PID_i).value = i_value;
-	document.getElementById(PID_d).value = d_value;
-	document.getElementById(PID_i_max).value = i_max_value;
+	parseInt(document.getElementById(PID_p).value) = p_value;
+	parseInt(document.getElementById(PID_i).value) = i_value;
+	parseInt(document.getElementById(PID_d).value) = d_value;
+	parseInt(document.getElementById(PID_i_max).value) = i_max_value;
+
+	console.log(parseInt(document.getElementById(PID_p).value));
 
 	const message = {command: "set_pid_values", value: {k_p: p_value, k_i: i_value, k_d: d_value, i_max: i_max_value} }
 	ws.send(JSON.stringify(message));
